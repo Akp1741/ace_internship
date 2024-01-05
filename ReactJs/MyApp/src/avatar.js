c25902c3db0cea0bf8e38b4bb7f5aee67e09aeb4
AyushPatel <110102619+Akp1741@users.noreply.github.com>
2024-01-05 11:04:44 +0530

    Delete ReactJs directory

--- ReactJs/MyApp/src/avatar.js
+++
@@ -1,8 +0,0 @@
-export default function Profile() {
-    return (
-      <Avatar
-        person={{ name: 'Lin Lanying', imageId: '1bX5QH6' }}
-        size={100}
-      />
-    );
-  }
--- ReactJs/MyApp/src/clock.js
+++
@@ -1,9 +0,0 @@
- function Clock({ color, time }) {
-    return (
-      <h1 style={{ color: color }}>
-        {time}
-      </h1>
-    );
-  }
-  export default Clock;
-  
